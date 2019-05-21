d8a9ea93c00a35208bd5105307466da7222d421b
carlos calderon <cdcalderon@gmail.com>
2019-05-21 01:22:45 -0500

    initial commit

---
+++ CreateStockQuote/CreateStockQuote.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+
+namespace CreateStockQuote
+{
+
+    public static class CreateStockQuote
+    {
+        [FunctionName("CreateStockQuote")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            Quote quote = new Quote();
+            quote.Symbol = "AAPL";
+
+            var uri = new Uri("https://investips2019.documents.azure.com:443/");
+            var key = "0AZIIWw8IENGZdFXZSzN4BzZvmvhom5xgVLzsIvbhZBItebHsz3y8XL4qdMCM4NSSwmFCMd4J2mCRD4FbHCZcw==";
+            var quotesLink = UriFactory.CreateDocumentCollectionUri("investips", "quotes");
+            var option = new FeedOptions { EnableCrossPartitionQuery = true };
+
+            var client = new DocumentClient(uri, key);
+
+            await client.CreateDocumentAsync(quotesLink, quote);
+
+            log.LogInformation("Saved Quote");
+
+            var justSavedQuote = client.CreateDocumentQuery<Quote>(quotesLink, option)
+                                       .OrderBy(q => q.Symbol).ToList();
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+
+    public class Quote
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+    }
+
+    public class QuoteRead
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+    }
+}
